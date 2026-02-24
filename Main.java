097f4ad4008226c36cbecf37b6a072848be19bef
Jagan Deepak <jd5436@srmist.edu.in>
2026-02-24 14:58:44 +0530

    UC2 Done

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int number = 10;   // local variable
-        System.out.println(number);
-    }
-}
--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,21 @@
+/**
+ * OOPSBannerApp
+ * UC2: Display OOPS in Banner Format
+ * @author Jagandeepak
+ * @version 2.0
+ */
+
 public class OOPSBannerApp {
-      public static void main(String[] args) {
-          System.out.println("OOPS");
-        }
-	}	
+
+    public static void main(String[] args) {
+
+        // O
+        System.out.println("  *****     *****     *****     *****  ");
+        System.out.println(" *     *   *     *   *     *   *     * ");
+        System.out.println(" *     *   *     *   *     *   *       ");
+        System.out.println(" *     *   ******    ******    ******* ");
+        System.out.println(" *     *   *         *               * ");
+        System.out.println(" *     *   *         *         *     * ");
+        System.out.println("  *****    *         *          *****  ");
+    }
+}
